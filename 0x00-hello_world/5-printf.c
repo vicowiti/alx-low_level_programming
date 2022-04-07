8baa072b689395b6e6a3fa80ffbf7723a5c49169
Victor Owiti <viowiti12@gmail.com>
2022-04-07 04:59:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - use printf to output a string
+ *
+ * return - 0 when successful, any other integer when unsuccessful
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
