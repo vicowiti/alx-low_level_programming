9ba9c8d4022c9f415ff88f23f38f78ba6bdfaba9
Victor Owiti <viowiti12@gmail.com>
2022-04-07 04:52:15 -0700

    output text to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
