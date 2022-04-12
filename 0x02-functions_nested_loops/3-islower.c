be84fe25d60da92c3e9ef849f665a0e7435943c9
Victor Owiti <viowiti12@gmail.com>
2022-04-12 07:53:28 -0700

    checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks if tthe param provided is a lowercase char
+ * Return: returns 1 if it is lowercase, o otherwise
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
