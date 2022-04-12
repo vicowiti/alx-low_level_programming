9b049c3ef1777e554479148aeeadf0a77d96bd2f
Victor Owiti <viowiti12@gmail.com>
2022-04-12 02:09:23 -0700

    print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print the alphabet
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i = 97;
+
+	for (; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
