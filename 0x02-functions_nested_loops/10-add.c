e101a809e5bf759850068988ad886121c7da29c6
Victor Owiti <viowiti12@gmail.com>
2022-04-12 14:16:28 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * Return: result of addition
+ * @n: param 1
+ * @x: param 2
+ */
+int add(int n, int x)
+{
+	return (n + x);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,16 @@
 int print_last_digit(int n)
 {
 	int last_num;
-	
+
 	if (n < 0)
 	{
 		n = n / -1;
 		last_num = n % 10;
 		return (last_num);
+	}
+	if (n < 10)
+	{
+		return (n);
 	}
 	last_num = n % 10;
 	return (last_num);
