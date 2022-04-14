ba5c8617e90df6c73e46794473ddc22fa2f4ed5f
Victor Owiti <viowiti12@gmail.com>
2022-04-14 13:19:49 -0700

    shortest distance

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,11 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			putchar('_');
 		}
 		putchar('\n');
 	}
-	else
-	{
-		putchar('\n');
-	}
+	putchar('\n');
 }
